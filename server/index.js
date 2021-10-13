9ff49bc5978250f2c95a3a54cfe74796fe54abf1
kimothokamau <kimothokamau@gmail.com>
2021-10-13 19:56:58 +0300

    Port to ports

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ res.json({ message: "Welcome to bezkoder application." });
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}.`);
+ console.log(`Server is running on ports ${PORT}.`);
 });
 
  function initial() {
