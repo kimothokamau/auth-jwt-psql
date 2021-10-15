4810c11431216722ba65f99cb4e5b051c8e4a6ba
kimothokamau <kimothokamau@gmail.com>
2021-10-15 19:31:44 +0300

    Edit package json run dev

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
                 </Link>
               </li>
               <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={this.logOut}>
+                <a href="/#/login" className="nav-link" onClick={this.logOut}>
                   LogOut
                 </a>
               </li>
