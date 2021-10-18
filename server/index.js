e762fe3c89340332421beca18625f045068ff67d
kimothokamau <kimothokamau@gmail.com>
2021-10-18 17:49:30 +0300

    DB syntax error

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const config = require("./config/db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const db = {};
+// const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
