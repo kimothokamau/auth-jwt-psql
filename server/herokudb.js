4d0a3ac4c7f9d7be12f4eedd04c415150b426a85
kimothokamau <kimothokamau@gmail.com>
2021-10-18 18:42:30 +0300

    remove port 5432 from host

--- server/herokudb.js
+++ server/herokudb.js
@@ -1,7 +1,7 @@
 const herokupsql = {
   username: 'ycinnjfulighiu',
   password: '5b554a02887ad5cd393efca05fa3b2e479541ba4fd2c527b7fe508b92803bdd4',
-  host: 'ec2-54-209-187-69.compute-1.amazonaws.com:5432',
+  host: 'ec2-54-209-187-69.compute-1.amazonaws.com',
   database: 'd8stc5de6buinl',
   dialect: 'postgres',
 }
