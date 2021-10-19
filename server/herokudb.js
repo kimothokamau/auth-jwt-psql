f4cb1443d76fcba6837fd98f6ad94cb306d06c19
kimothokamau <kimothokamau@gmail.com>
2021-10-19 21:38:52 +0300

    heroku db config

--- server/herokudb.js
+++ server/herokudb.js
@@ -1,8 +1,8 @@
 const herokupsql = {
-  user: 'ezawnmbupwxren',
-  password: 'dabd60994cee270352c28909d9af2413bf89bf15419fcaea09d9483ef96d2976',
-  host: 'ec2-54-209-187-69.compute-1.amazonaws.com',
-  database: 'dbajr29klkg3gh',
+  user: 'mpqreaodzpnbqm',
+  password: 'cb20d0912cfbf832deb8b6f2276fcb51654b2de44e6366f7f25fcc731ff53598',
+  host: 'ec2-44-195-240-222.compute-1.amazonaws.com',
+  database: 'd5e43n9p6rg842',
   dialect: 'postgres',
 }
 
