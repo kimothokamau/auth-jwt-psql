3156cc59b3ba83045f287960e1446a91066d84c0
kimothokamau <kimothokamau@gmail.com>
2021-10-19 06:28:45 +0300

    heroku db

--- server/herokudb.js
+++ server/herokudb.js
@@ -1,8 +1,8 @@
 const herokupsql = {
-  user: 'ycinnjfulighiu',
-  password: '5b554a02887ad5cd393efca05fa3b2e479541ba4fd2c527b7fe508b92803bdd4',
+  user: 'ezawnmbupwxren',
+  password: 'dabd60994cee270352c28909d9af2413bf89bf15419fcaea09d9483ef96d2976',
   host: 'ec2-54-209-187-69.compute-1.amazonaws.com',
-  database: 'd8stc5de6buinl',
+  database: 'dbajr29klkg3gh',
   dialect: 'postgres',
 }
 
